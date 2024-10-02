6085146f25a1900422c1248aeb326ecc3bae42c3
jiepengtan <jiepengtan@gmail.com>
2024-10-02 14:40:27 +0800

    Add basic ui node

--- internal/engine/sync.go
+++ internal/engine/sync.go
@@ -1,5 +1,20 @@
 package engine
 
+import (
+	. "godot-ext/gdspx/pkg/engine"
+)
+
 func SyncInputMousePressed() bool {
 	return SyncInputGetMouseState(0) || SyncInputGetMouseState(1)
+}
+func SyncCreateUiNode[T any](path string) *T {
+	var __ret *T
+	done := make(chan struct{})
+	job := func() {
+		__ret = CreateUI[T](path)
+		done <- struct{}{}
+	}
+	updateJobQueue <- job
+	<-done
+	return __ret
 }
