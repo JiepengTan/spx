1a8b0a820d7616cff43bf9960120d8896d26e301
jiepengtan <jiepengtan@gmail.com>
2024-10-09 13:42:31 +0800

    Add clamp01

--- internal/engine/math.go
+++ internal/engine/math.go
@@ -7,3 +7,31 @@ import (
 func NewVec2(x, y float64) Vec2 {
 	return Vec2{X: float32(x), Y: float32(y)}
 }
+
+func Clamp01d(val float64) float64 {
+	if val < 0 {
+		val = 0
+	}
+	if val > 1 {
+		val = 1
+	}
+	return val
+}
+func Clamp01(val float32) float32 {
+	if val < 0 {
+		val = 0
+	}
+	if val > 1 {
+		val = 1
+	}
+	return val
+}
+func Clamp01i(val int64) int64 {
+	if val < 0 {
+		val = 0
+	}
+	if val > 1 {
+		val = 1
+	}
+	return val
+}
