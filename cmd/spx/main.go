e7a9ce3764f50d043960aa1a92a09c1346cd5adb
jiepengtan <jiepengtan@gmail.com>
2024-10-24 08:55:51 +0800

    Add cmd: runi & exporti

--- cmd/spx/main.go
+++ cmd/spx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"embed"
 	"encoding/json"
 	"fmt"
@@ -8,8 +9,10 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 
@@ -84,24 +87,95 @@ func execCmds() error {
 	impl.CopyEmbed(engineFiles, "template/engine", filepath.Join(impl.TargetDir, "engine"))
 	webDir := path.Join(impl.ProjectPath, ".builds/web")
 	var err error = nil
-	switch os.Args[1] {
-	case "exportspx":
-		err = impl.ExportWebEditor(impl.GdspxPath, impl.ProjectPath, impl.LibPath)
-		impl.PackProject(impl.TargetDir, path.Join(webDir, "game.zip"))
-		impl.SetupFile(true, path.Join(webDir, "index.html"), index_html)
-		return err
-	case "runweb":
-		if len(os.Args) > 3 {
-			port := os.Args[3]
-			impl.ServerPort, _ = strconv.Atoi(port)
-			newText := strings.Replace(index_html, "127.0.0.1:8005", "127.0.0.1:"+port, -1)
-			os.WriteFile(path.Join(webDir, "index.html"), []byte(newText), 0)
-		}
-	}
 	err = impl.ExecCmds(buildDll)
+	switch os.Args[1] {
+	case "exporti":
+		return exportInterpreterMode(webDir)
+	case "runi":
+		return runInterpreterMode(webDir)
+	}
+	return err
+}
+func exportInterpreterMode(webDir string) error {
+	err := impl.ExportWebEditor(impl.GdspxPath, impl.ProjectPath, impl.LibPath)
+	packProject(impl.TargetDir, path.Join(webDir, "game2.zip"))
+	impl.SetupFile(true, path.Join(webDir, "index.html"), index_html)
+	impl.BuildWasm(impl.TargetDir)
+	// convert to the correct zip format
+	if runtime.GOOS == "windows" {
+		os.Chdir(webDir)
+		exec.Command("WinRAR.exe", "x", "./game2.zip", "./").Run()
+		exec.Command("WinRAR.exe", "a", "-afzip", "-ep1", "./game.zip", "./game").Run()
+		os.Remove("./game2.zip")
+		os.RemoveAll("./game")
+		os.Chdir("../..")
+	} else {
+		panic("TODO: Compress the project into the correct zip format. " + runtime.GOOS)
+	}
 	return err
 }
 
+func runInterpreterMode(webDir string) error {
+	if len(os.Args) > 3 {
+		port := os.Args[3]
+		impl.ServerPort, _ = strconv.Atoi(port)
+		newText := strings.Replace(index_html, "127.0.0.1:8005", "127.0.0.1:"+port, -1)
+		os.WriteFile(path.Join(webDir, "index.html"), []byte(newText), 0)
+	}
+	return impl.RunWebServer(impl.GdspxPath, impl.ProjectPath, impl.LibPath, impl.ServerPort)
+}
+
+func packProject(baseFolder string, dstZipPath string) {
+	if impl.IsFileExist(dstZipPath) {
+		os.Remove(dstZipPath)
+	}
+	skipDirs := []string{"lib", ".godot", ".builds"}
+	file, err := os.Create(dstZipPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	zipWriter := zip.NewWriter(file)
+	defer zipWriter.Close()
+
+	filepath.Walk(baseFolder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		for _, skipDir := range skipDirs {
+			if info.IsDir() && info.Name() == skipDir {
+				return filepath.SkipDir
+			}
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		fileToZip, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer fileToZip.Close()
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		header.Name = "game/" + strings.TrimPrefix(path, baseFolder)
+		header.Name = strings.ReplaceAll(header.Name, "\\", "/")
+		header.Method = zip.Store
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(writer, fileToZip)
+		return err
+	})
+}
 func showHelpInfo() {
 	impl.ShowHelpInfo("spx")
 }
