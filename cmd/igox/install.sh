f27bcab5f48b6b1ae6005daceb293c485520c139
jiepengtan <jiepengtan@gmail.com>
2025-05-21 10:40:26 +0800

    aa

--- cmd/igox/install.sh
+++ cmd/igox/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Read app name from appname.txt file
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $SCRIPT_DIR
+
 go mod tidy
-GOOS=js GOARCH=wasm go build -tags canvas -ldflags -checklinkname=0  -o igdspx.wasm
+GOOS=js GOARCH=wasm go build -tags canvas -ldflags -checklinkname=0  -o gdspx.wasm
