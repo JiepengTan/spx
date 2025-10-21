83cf96adec570b111b5bec06c13763c289742fa9
jiepengtan <jiepengtan@gmail.com>
2025-10-21 12:35:47 +0800

    aa

--- cmd/gox/pkg/command/args.go
+++ cmd/gox/pkg/command/args.go
@@ -149,7 +149,6 @@ func (cmd *CmdTool) parseCommandLineArgs(help *bool, ext ...string) error {
 
 	if cmd.Args.AiPack != nil && *cmd.Args.AiPack == "default" {
 		cmd.Args.AiPack = &DefaultAiPackVersion
-		println("aaa cmd.Args.AiPack = ", *cmd.Args.AiPack)
 	}
 	return nil
 }
